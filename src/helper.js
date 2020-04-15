48ce50fc34ab3b0d8956fcbd50187222c9122fc0
AlexGlz <alex.glz.v@hotmail.com>
2020-04-15 16:35:23 -0500

    Sprint 1 (#6)
    * Sprint 1
    
    * Helper.js correction

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports.baseURL='https://inscripcionespbi-backend.herokuapp.com'
+
 }else{
     module.exports.baseURL='http://localhost:3000'
+
 }

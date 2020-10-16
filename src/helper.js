9c7c80bedba0718ecc1d05ecc4b831982e655fed
Rick <garzag.rick@gmail.com>
2020-10-16 11:50:03 -0500

    Cambio de API (#45)
    Se cambia el link de Heroku para poder empezar a usarse en línea.

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-	module.exports.baseURL = "https://pbi-mty.herokuapp.com/";
+	module.exports.baseURL = "https://pbi-mty.herokuapp.com";
 } else {
 	module.exports.baseURL = "http://localhost:3000";
 }
